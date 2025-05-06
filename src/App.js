b558381171ba2bc90b80e8d5c618d688aee21aae
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2025-05-06 22:01:10 +0300

    Test chart updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { render } from "react-dom";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 function App() {
+  const options = {
+    title: {
+      text: "My chart",
+    },
+    series: [
+      {
+        data: [1, 2, 3],
+      },
+    ],
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 }
